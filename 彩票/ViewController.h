a8238f823b2a25d8c2e2950bd19a9b5225c44ec1
Jusive <1345266022@qq.com>
2015-11-30 22:09:04 +0800

    Initial Commit

---
+++ 彩票/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  彩票
+//
+//  Created by Jusive on 15/11/30.
+//  Copyright © 2015年 Jusive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
